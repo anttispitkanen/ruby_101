7e5f5db5f990ab38908ca3cc76056538ad70ba14
Antti <anttis.pitkanen@gmail.com>
2016-02-23 23:23:55 +0200

    logic works better now

--- mastermind/lib/mastermind/board.rb
+++ mastermind/lib/mastermind/board.rb
@@ -10,26 +10,31 @@ module Mastermind
       @guess = Row.new
     end
 
-#    def get_computers_row
-#      return @computers_row
-#    end
-
     def make_a_guess
       #@guess = Row.new.choose_numbers_manually
       @guess.clear_row
       @guess.choose_numbers_manually
-      #puts @guess.row_numbers
       check_row
       #victory here
       @guesses_left-=1
-      puts "You have #{@guesses_left} guesses left." if guesses_left?
+      puts "\nYou have #{@guesses_left} guesses left.\n\n" if guesses_left?
     end
 
     def guesses_left?
       @guesses_left > 0 ? true : false
     end
 
+    def check_row
+      print "Corrects: ", check_absolute_corrects, "\n"
+      #victory if four of these
+      print "Semis: ", check_semi_corrects, "\n"
+      print "computer: ", @computers_row, "\n"
+      print "guess: ", @guess.row_numbers, "\n"
+      #return number_of_absolute_corrects
+      #print "corrects: ", number_of_absolute_corrects, "\n"
+    end
 
+=begin
     def check_row
       number_of_absolute_corrects = 0
       number_of_not_absolute_corrects = 0
@@ -38,7 +43,6 @@ module Mastermind
         if @computers_row[i] == @guess.row_numbers[i]
           number_of_absolute_corrects+=1
         end
-        #puts "testing #{i}"
         i+=1
       end
       print "computer: ", @computers_row, "\n"
@@ -46,6 +50,42 @@ module Mastermind
       #return number_of_absolute_corrects
       print "corrects: ", number_of_absolute_corrects, "\n"
     end
+=end
+
+    def check_absolute_corrects
+      number_of_absolute_corrects = 0
+      print "\n\nGreetings from check_absolute_corrects, computers_row here is: "
+      (0...@computers_row.size).each do |i|
+        print @computers_row[i]
+        if @computers_row[i] == @guess.row_numbers[i]
+          number_of_absolute_corrects+=1
+        end
+      end
+      print "\n\n"
+      return number_of_absolute_corrects
+    end
+
+    def check_semi_corrects
+      mock_computers_row = Array.new(@computers_row)
+      mock_guess = Array.new(@guess.row_numbers)
+      number_of_semi_corrects = 0
+
+      (0...mock_computers_row.size).each do |i|
+        if mock_computers_row[i] == mock_guess[i]
+          mock_computers_row[i] = nil
+          mock_guess[i] = -1
+        end
+      end
+
+      mock_computers_row.each do |num|
+        if mock_guess.any? {|x| x == num}
+          number_of_semi_corrects+=1
+          #mock_computers_row.delete(num)
+        end
+      end
+      return number_of_semi_corrects
+    end
+
 
   end
 end
